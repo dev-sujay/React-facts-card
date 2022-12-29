94366417e66c8c901be5d56561759d100936bfd9
Sujay Paul <sujaypaul531@gmail.com>
2022-12-29 22:35:49 +0530

    uploading

---
+++ components/main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+    return (
+        <div className="main">
+            <h1 className="heading">Fun facts about React</h1>
+            <ul className="facts">
+                <li>Was released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Maintained by Facebook</li>
+                <li>Powers thousands on enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Main from "/components/main"
+import './index.css'
+
+function App() {
+  return (
+    <section>
+      <a href="https://reactjs.org/">
+        <img src="/src/assets/icon.png" alt="" className="logo" />
+      </a>
+      <Main />
+    </section>
+  )
+}
+
+export default App
